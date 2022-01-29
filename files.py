69363d6e206dd0f5fc4d2f43ae18ca872095372a
mohaned-omar <eslamoomar007@gmail.com>
2022-01-29 22:04:07 +0200

    done

--- files.py
+++ files.py
@@ -4,12 +4,14 @@ import datetime
 day =(datetime.datetime.now()-datetime.timedelta(hours=3)).strftime("%Y-%m-%d") 
 
 def create_files():
-    files = ['','photos', 'logo','frame']
+    files = ['photos', 'logo','frame']
+    try:
+        os.mkdir('import')
+    except:
+        pass
+    
+    
     for i in files:
-        try:
-            os.mkdir('import')
-        except:
-            pass
         try:    
             os.mkdirs('import/'+i)
         except:
